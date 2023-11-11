5e9929f3aa9c667c0fe132b702c3e3b95e0f9c98
mhawryluk <marcinhawrylukxx@gmail.com>
2023-11-11 18:56:53 +0100

    Init

--- city-tour-ar/ContentView.swift
+++ city-tour-ar/ContentView.swift
@@ -10,7 +10,7 @@ import RealityKit
 
 struct ContentView : View {
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+            ARViewContainer().edgesIgnoringSafeArea(.all)
     }
 }
 
