67aa59a23c424baec79e97e3054a683ef5e75fea
mhawryluk <marcinhawrylukxx@gmail.com>
2024-03-26 16:11:00 +0100

    Implement example Image Tracking

--- city-tour-ar/ContentView.swift
+++ city-tour-ar/ContentView.swift
@@ -8,11 +8,12 @@
 import SwiftUI
 import RealityKit
 import MapKit
+import ARKit
 
 struct ContentView : View {
     
     @State var showingChallenge = false
-    @State var showingMap = true
+    @State var showingMap = false
     
     var body: some View {
         NavigationStack {
@@ -89,30 +90,95 @@ struct MapView : View {
     }
 }
 
+class ARCoordinator: NSObject, ARSCNViewDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+            let node = SCNNode()
+        
+            // Perform tasks on the background thread
+            DispatchQueue.global().async { [weak self] in
+        
+                // Check if the detected anchor is an ARImageAnchor
+                if let imageAnchor = anchor as? ARImageAnchor {
+                    
+                    // Creating a plane geometry
+                    let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+                    
+                    plane.firstMaterial?.diffuse.contents = UIColor.blue.withAlphaComponent(0.3)
+                    
+                    // Creating a plane node
+                    let planeNode = SCNNode(geometry: plane)
+                    
+                    // Rotate the plane node 90 degrees counter clockwise
+                    planeNode.eulerAngles.x = -.pi / 2
+                    
+                    // Adding nodes on the main thread
+                    DispatchQueue.main.async {
+                        node.addChildNode(planeNode)
+                    }
+                }
+            }
+            
+            return node
+        }
+        
+        func session(_ session: ARSession, didFailWithError error: Error) {
+            // Present an error message to the user
+            
+        }
+        
+        func sessionWasInterrupted(_ session: ARSession) {
+            // Inform the user that the session has been interrupted, for example, by presenting an overlay
+            
+        }
+        
+        func sessionInterruptionEnded(_ session: ARSession) {
+            // Reset tracking and/or remove existing anchors if consistent tracking is required
+            
+        }
+    
+}
+
 
 struct ARViewContainer: UIViewRepresentable {
     
-    func makeUIView(context: Context) -> ARView {
+    func makeUIView(context: Context) -> some UIView {
         
-        let arView = ARView(frame: .zero)
+        let sceneView = ARSCNView(frame: .zero)
+        sceneView.showsStatistics = true
+        sceneView.delegate = context.coordinator
 
-        // Create a cube model
-        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-        let model = ModelEntity(mesh: mesh, materials: [material])
+//        // Create a cube model
+//        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+//        let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
+//        let model = ModelEntity(mesh: mesh, materials: [material])
+//
+//        // Create horizontal plane anchor for the content
+//        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+//        anchor.children.append(model)
+//
+//        // Add the horizontal plane anchor to the scene
+//        arView.scene.anchors.append(anchor)
+        
+        
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
+            fatalError("Missing expected asset catalog resources.")
+        }
 
-        // Create horizontal plane anchor for the content
-        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-        anchor.children.append(model)
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.detectionImages = referenceImages
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
 
-        // Add the horizontal plane anchor to the scene
-        arView.scene.anchors.append(anchor)
-
-        return arView
+        return sceneView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: UIViewType, context: Context) {}
+    
+
+    func makeCoordinator() -> ARCoordinator {
+        ARCoordinator()
+    }
     
 }
 
